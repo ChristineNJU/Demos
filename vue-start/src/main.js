55d6ccba2ad725ace988b1f3a92290618ec14ace
christine <christine@christinedeMacBook-Pro.local>
2017-07-14 11:47:30 +0800

    new project for learing NodeJS

--- vue-start/src/main.js
+++ vue-start/src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 import '../theme/index.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
